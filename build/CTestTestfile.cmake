f3ec668e719adf57b40ff28ca9afbde286e81e17
Danil Shvalov <shvalovich@gmail.com>
2021-01-29 14:04:42 +1000

    update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/danilshvalov/dev/c-plus-plus/algorithms
-# Build directory: /home/danilshvalov/dev/c-plus-plus/algorithms/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/greedy-algorithms/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/danilshvalov/dev/c-plus-plus/algorithms/greedy-algorithms
-# Build directory: /home/danilshvalov/dev/c-plus-plus/algorithms/build/greedy-algorithms
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
